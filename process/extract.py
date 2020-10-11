1febb5a9e241b516af4a3c9c8330cf216d1b142f
MestafaKamal <em_ben_ramdane@esi.dz>
2020-10-11 11:22:52 +0100

    texts

---
+++ process/extract.py
@@ -0,0 +1,23 @@
+# created by Mestafa Kamal
+
+import sys
+import os
+import shutil
+import argparse
+import csv
+import text_cleaning as tc
+
+fileName="../corrected/Trad-n-Yugurten-Traduc-M.O-Kheddam.pdf.txt"
+
+vocabulary = open("result.txt", "wt", encoding="utf-8")
+with open(fileName, "rt", encoding="utf-8") as originalFile:
+    i = 0
+    for row in originalFile:
+        sentence = row.replace("\n", "")
+        cleanedSentence = tc.cleanSentence(sentence)
+        print("S :", sentence)
+        print("CS:", cleanedSentence)
+        vocabulary.write(cleanedSentence + "\n")        
+
+originalFile.close()
+vocabulary.close()
